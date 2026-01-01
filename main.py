7c00e35b133e27dc3e9e43e0d61eb69c65aaeca1
pimpnamek-svg <pimpnamek@gmail.com>
2026-01-01 09:46:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ Railway start command:
 # ==========================
 # APP
 # ==========================
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from typing import Optional
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=HTMLResponse)
